da0cfed90669e0099c66dd42261b35fc9c22c998
Tinsley Devers <Tinsley.devers@Gmail.com>
2025-01-06 07:54:00 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,17 @@ const axios = require("axios");
 const cors = require("cors");
 
 const app = express();
+
 const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
 const RIOT_API_KEY = process.env.RIOT_API_KEY;
 
 app.use(cors());
